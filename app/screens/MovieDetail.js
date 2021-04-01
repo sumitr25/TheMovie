9c3c84b8a7533a146dae06f93c60250b3f03e5c2
Sumit <grt.sumitr25@gmail.com>
2021-04-01 14:04:21 +0530

    Add Movie Detail screen

--- app/screens/MovieDetail.js
+++ app/screens/MovieDetail.js
@@ -1,16 +1,48 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import colors from '../res/colors';
+import fonts from '../res/fonts';
 
-const MovieDetail = () => {
+const MovieDetail = ({ route }) => {
+  const { id } = route.params;
+
   return (
     <View style={styles.container}>
-      <Text>Movie Detail</Text>
+      <Image
+        style={styles.banner}
+        source={{
+          uri: 'https://reactnative.dev/img/tiny_logo.png',
+        }}
+      />
+
+      <Text style={styles.title}>Title</Text>
+      <Text style={styles.description}>
+        If you use react-navigation v5, you should import hooks from
+        react-navigation v5 directly, and should not add this project.
+      </Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: { flex: 1, backgroundColor: 'white' },
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    paddingHorizontal: fonts.large,
+    paddingVertical: fonts.small,
+  },
+  banner: {
+    height: 200,
+  },
+  title: {
+    color: colors.black,
+    fontWeight: fonts.bold,
+    fontSize: fonts.normal,
+    paddingVertical: fonts.small,
+  },
+  description: {
+    color: colors.black,
+  },
 });
 
 export default MovieDetail;
